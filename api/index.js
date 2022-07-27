4a7c9fd9f92f584fd283312ce24c0fd0c0daf135
baraka <barakaganai01@gmail.com>
2022-07-27 13:30:05 +0300

    write index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen("4000", ()=> {
-    console.log("backend is running");
-})
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("connected to MONGODB"))
+  .catch((err) => console.log("error"));
+
+app.use("/", (req, res) => {
+  console.log("hey this rama url");
+});
+
+app.listen("4000", () => {
+  console.log("backend is running");
+});
