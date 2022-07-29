42b8fa5d0e7fa6da5fbf5023c2c8462a7b45a317
baraka <barakaganai01@gmail.com>
2022-07-30 00:55:50 +0300

    get post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -57,4 +57,14 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+//GET POST
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json("post");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
