41d8dd7eceadf1621051234aa29c7e20b3e38a24
baraka <barakaganai01@gmail.com>
2022-07-30 09:51:08 +0300

    indicate category and multer for image storage

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 const app = express();
 
 dotenv.config();
@@ -14,13 +16,24 @@ mongoose
   .then(console.log("connected to MONGODB"))
   .catch((err) => console.log("error"));
 
-app.use("/", (req, res) => {
-  console.log("hey this rama url");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cd(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cd(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
 });
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("4000", () => {
   console.log("backend is running");
