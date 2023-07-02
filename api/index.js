53c920049f740b90f4576b612971d1e644c575ba
barakaganai <barakaganai01@gmail.com>
2023-07-02 13:17:40 +0300

    prohibit mongoose warning

--- api/index.js
+++ api/index.js
@@ -21,6 +21,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
+mongoose.set("strictQuery", false);
+
 //mongodb connection
 const connect = async () => {
   try {
